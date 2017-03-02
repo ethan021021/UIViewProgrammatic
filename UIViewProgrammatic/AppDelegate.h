a95027d152b5a8900e9e2543262b44079664f2a1
Ethan Thomas <ethan021@me.com>
2017-03-01 22:04:10 -0800

    Initial Commit

---
+++ UIViewProgrammatic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIViewProgrammatic
+//
+//  Created by Diamond on 3/1/17.
+//  Copyright © 2017 ethanthomas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIViewProgrammatic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIViewProgrammatic
+//
+//  Created by Diamond on 3/1/17.
+//  Copyright © 2017 ethanthomas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
